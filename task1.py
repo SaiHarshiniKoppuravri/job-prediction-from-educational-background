6d7aee20d9e28069f963a89bd6aed1fd4c73ccfb
Koppuravuri Devi Venkata Sai Harshini <harshini1732@gmail.com>
2025-10-16 19:15:51 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+# Program to check whether a number is even or odd
+num = int(input("Enter a number: "))
+# Check if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
---
+++ task2.py
@@ -0,0 +1,4 @@
+# Program to create a list of squares using list comprehension
+# Create a list of squares from 1 to 10
+squares = [x**2 for x in range(1, 11)]
+print("List of squares:", squares)
